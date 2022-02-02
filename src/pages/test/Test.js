bb2e8f2d86280f3f13e323abbcaea6aa706bb60c
semtrip <75387322+semtrip@users.noreply.github.com>
2022-02-03 01:02:07 +0700

    upload

--- src/pages/test/Test.js
+++ src/pages/test/Test.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { observer } from "mobx-react-lite";
 import './style.scss';
 
@@ -5,19 +6,111 @@ import TestStore from './index'
 
 const Test = observer(()=>{
     const state = TestStore.state
+    const {eventHandler} = TestStore
+    let [path, setPath] = useState(0)
+    let [navPath, setNavPath] = useState(0)
+    let [searchData, setSearchData] = useState('')
+    const isAccess = state.playerLVL >= state.items[path].access
+    const typeBlock = state.items[path].type
+
     return state.show ?(
         <div className="test">
             <div className="container">
+                <span className="playerAcces">Your access level: {state.playerLVL}</span>
                 <div className="head">
                     {
                         state.items.map((item, i)=>(
-                            <div className="btn" key={i}><span>{item.btn_name}</span></div>
+                            <div className={path === i ? 'btn active': 'btn'} key={i} onClick={()=>{setPath(i); setNavPath(0)}}><span>{item.title}</span></div>
                         ))
                     }
                 </div>
-                <div className="content">
+                <div className="top">
+                            <div className="text">
+                                <span className="title">
+                                    {state.items[path].title}
+                                </span>
+                                <span className="access">
+                                    {state.items[path].access}-уровень
+                                </span>
+                            </div>
+                            {isAccess  && 
+                            typeBlock === 'list' ?
+                                <div className="search">
+                                    <input type="text" placeholder="Search..." onChange={(e)=> setSearchData(e.target.value)}/>
+                                    <div className="btn"/>
+                                </div>:null
+                            }
 
                 </div>
+                {
+                    isAccess ?
+                    <div className="page">
+                        {
+                            typeBlock === 'list' ?
+                            <div className="content">
+                                <div className="nav">
+                                    {
+                                        state.items[path].data.map((item, i) => (
+                                            <div className={navPath === i ? `item active ${item.ico}`: `item ${item.ico}` } key={i} onClick={()=>setNavPath(i)}><span>{item.title}</span></div>
+                                        ))
+                                    }
+                                </div>
+                                <div className="text-block">
+                                    {
+                                        state.items[path].data[navPath].content.map((item, i) => (
+                                            item.title.toLowerCase().includes(searchData) ||
+                                            item.description.toLowerCase().includes(searchData) ||
+                                            item.text.toLowerCase().includes(searchData) ? 
+                                            <div key={i}>
+                                                <span className="title"key={i}>{item.title}</span>
+                                                <span className="description">{item.description}</span>
+                                                <p>{item.text}</p>
+                                            </div>:null
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                            :null
+                        }
+                        {
+                            typeBlock === 'btns' ? 
+                            <div className="content">
+                                {
+                                    state.items[path].data.map((item, i)=>(
+                                        <div className={`item-btn bg_${item.bg}`} key={i}>
+                                            <span>{item.text}</span>
+                                            {
+                                                item.btns !== undefined ?
+                                                <div className="item-btn-additional">
+                                                    {
+                                                        item.btns.map((el, b)=>(
+                                                            <div className="additional-btn" key={b} onClick={(e) => el.confir !== undefined ? e.target.classList.add('active') :eventHandler('click btn', [i,b])}>
+                                                                <span className="name">{el.name}</span>
+                                                                {
+                                                                    el.confir !== undefined ?
+                                                                    <div className="confirmation">
+                                                                        <span className="answer no" onClick={(e)=> {e.target.parentNode.parentNode.classList.remove('active'); eventHandler('click btn confirmation no', [i,b])}}>- Нет</span>
+                                                                        <span className="answer ok" onClick={(e)=> {e.target.parentNode.parentNode.classList.remove('active'); eventHandler('click btn confirmation ok', [i,b])}}>- Ok</span>
+                                                                    </div>:null 
+                                                                }
+                                                                
+                                                            </div>
+                                                        ))
+                                                    }
+                                                </div>:null
+                                            }
+                                        </div>
+                                    ))
+                                } 
+                            </div>
+                            :null
+                        }
+                    </div>:
+                    <div className="noAccess">
+                        <span className="noAcces-text">У ВАС НЕТ ДОСТУПА , ПОВЫСЬТЕ УРОВЕНЬ ЧТОБЫ ПОЛЬЗОВАТЬСЯ ЭТИМ МЕНЮ</span>
+                    </div>
+                }
+
             </div>
         </div>
     ):null
--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -4,30 +4,73 @@ class TestStore {
     constructor() {
         makeAutoObservable(this, {}, { deep: true })
     }
-    state ={
+    state = {
         show: true,
-        playerLVL: 1,
-        items:[
+        playerLVL: 7,
+        items: [
             {
-                btn_name: 'Памятка 1',
+                title: 'Памятка 1',
+                access: 1,
+                type: 'list',
+                data: [
+                    { title: 'title 1', ico: 'ico_1', content: [{ title: 'title', description: 'description', text: 'Lorem1, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem inventore hic ad aspernatur nesciunt itaque excepturi, nam sapiente minima ullam repellat consectetur obcaecati quod. Fugiat distinctio ipsam inventore similique quibusdam!' }, { title: 'title', description: 'description', text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem inventore hic ad aspernatur nesciunt itaque excepturi, nam sapiente minima ullam repellat consectetur obcaecati quod. Fugiat distinctio ipsam inventore similique quibusdam!' }, { title: 'title1', description: 'description', text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem inventore hic ad aspernatur nesciunt itaque excepturi, nam sapiente minima ullam repellat consectetur obcaecati quod. Fugiat distinctio ipsam inventore similique quibusdam!' }, { title: 'title', description: 'description', text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem inventore hic ad aspernatur nesciunt itaque excepturi, nam sapiente minima ullam repellat consectetur obcaecati quod. Fugiat distinctio ipsam inventore similique quibusdam!' }, { title: 'title', description: 'description', text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem inventore hic ad aspernatur nesciunt itaque excepturi, nam sapiente minima ullam repellat consectetur obcaecati quod. Fugiat distinctio ipsam inventore similique quibusdam!' }, { title: 'title', description: 'description', text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem inventore hic ad aspernatur nesciunt itaque excepturi, nam sapiente minima ullam repellat consectetur obcaecati quod. Fugiat distinctio ipsam inventore similique quibusdam!' }, { title: 'title', description: 'description', text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem inventore hic ad aspernatur nesciunt itaque excepturi, nam sapiente minima ullam repellat consectetur obcaecati quod. Fugiat distinctio ipsam inventore similique quibusdam!' }, { title: 'title', description: 'description', text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem inventore hic ad aspernatur nesciunt itaque excepturi, nam sapiente minima ullam repellat consectetur obcaecati quod. Fugiat distinctio ipsam inventore similique quibusdam!' }, { title: 'title', description: 'description', text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem inventore hic ad aspernatur nesciunt itaque excepturi, nam sapiente minima ullam repellat consectetur obcaecati quod. Fugiat distinctio ipsam inventore similique quibusdam!' }, { title: 'title', description: 'description', text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem inventore hic ad aspernatur nesciunt itaque excepturi, nam sapiente minima ullam repellat consectetur obcaecati quod. Fugiat distinctio ipsam inventore similique quibusdam!' },] },
+                    { title: 'title 2', ico: 'ico_2', content: [{ title: 'title', description: 'description', text: 'lorem' }] },
+                    { title: 'title 3', ico: 'ico_3', content: [{ title: 'title', description: 'description', text: 'lorem' }] },
+                    { title: 'title 4', ico: 'ico_4', content: [{ title: 'title', description: 'description', text: 'lorem' }] },
+                    { title: 'title 4', ico: 'ico_5', content: [{ title: 'title', description: 'description', text: 'lorem' }] },
+                    { title: 'title 4', ico: 'ico_6', content: [{ title: 'title', description: 'description', text: 'lorem' }] },
+                    { title: 'title 4', ico: 'ico_7', content: [{ title: 'title', description: 'description', text: 'lorem' }] },
+                    { title: 'title 4', ico: 'ico_8', content: [{ title: 'title', description: 'description', text: 'lorem' }] },
+                ]
             },
             {
-                btn_name: 'Памятка 2',
+                title: 'Памятка 2',
+                access: 2,
+                type: 'list',
+                data: [
+                    { title: 'title', content: [{ title: 'title', description: 'description', text: 'lorem' }] }
+                ]
             },
             {
-                btn_name: 'Памятка 3',
+                title: 'Памятка 3',
+                access: 3,
+                type: 'list',
+                data: [
+                    { title: 'title', content: [{ title: 'title', description: 'description', text: 'lorem' }] }
+                ]
             },
             {
-                btn_name: 'Памятка 4',
+                title: 'Памятка 4',
+                access: 4,
+                type: 'list',
+                data: [
+                    { title: 'title', content: [{ title: 'title', description: 'description', text: 'lorem' }] }
+                ]
             },
             {
-                btn_name: 'Памятка 5',
+                title: 'Памятка 5',
+                access: 5,
+                type: 'list',
+                data: [
+                    { title: 'title', content: [{ title: 'title', description: 'description', text: 'lorem' }] }
+                ]
             },
             {
-                btn_name: 'Памятка 6',
+                title: 'Памятка 6',
+                access: 6,
+                type: 'btns',
+                data: [
+                    {bg:'one', text:'Какой-то текст 1', btns:[{name:'Btn 1'},{name:'Btn 2', confir: true},{name:'Btn 3'},{name:'Btn 4', confir: true}]},
+                    {bg:'two', text:'Какой-то текст 2', btns:[{name:'Btn 1'},{name:'Btn 2', confir: true},{name:'Btn 3'},{name:'Btn 4', confir: true}]},
+                    {bg:'three', text:'Какой-то текст 3', btns:[{name:'Btn 1'},{name:'Btn 2', confir: true},{name:'Btn 3'},{name:'Btn 4', confir: true}]},
+                    {bg:'four', text:'Какой-то текст 4', btns:[{name:'Btn 1'},{name:'Btn 2', confir: true},{name:'Btn 3'},{name:'Btn 4', confir: true}]}
+                ]
             }
         ]
 
+    }
+    eventHandler = (event, data) =>{
+        console.log(event, 'block id:', data[0], 'btn id:', data[1])
     }
 }
 export default new TestStore()
